c76c46da16f6db3061c725767ac3b13e4d5ccc79
sergio santiago <sergiosantiago0403@gmail.com>
2025-03-13 10:11:29 +0100

    Fix: translated comments

--- app/containers/router.py
+++ app/containers/router.py
@@ -8,14 +8,15 @@ manager = ContainerManager()
 
 @container_router.post("/")
 async def lauch_acl2_container(containerUp: ContainerUp):
+    """ Endpoint to launch an ACL2 instance"""
     id = containerUp.container_id
     status = await manager.start_acl2_container(id)
     return status
 
 @container_router.post("/execute/")
 async def execute_acl2(commandRequest: CommandRequest):
-    """ Endpoint para ejecutar un comando en ACL2 dentro del contenedor y devolver la salida """
-    logger.info(f"Ejecutando el comando: {commandRequest.command} en el contenedor: {commandRequest.container_id}")
+    """ Endpoint to execute an ACL2 command and get the response """
+    logger.info(f"Running the command: {commandRequest.command} in container: {commandRequest.container_id}")
     cmd = commandRequest.command
     id = commandRequest.container_id
     #output = manager.send_acl2_command(id, cmd)
