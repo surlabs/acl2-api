a9475e2e76ee306053628c6b32573c8d8b5c51a3
Sergio Santiago <sergiosantiago0403@gmail.com>
2025-03-19 12:14:53 +0100

    Feat: test the request

--- app/containers/router.py
+++ app/containers/router.py
@@ -1,19 +1,24 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, Request
 from containers.container_manager import ContainerManager
 from containers.docker_container_manager import DockerContainerManager
 from core.logger import logger
 from .models.models import CommandRequest, ContainerUp, CommandResponse
 from api.websocket_manager import ws_manager
 from core.config import settings
+import json
 
 container_router = APIRouter()
 manager = ContainerManager()
 manager_docker = DockerContainerManager()
 
 @container_router.post("/", response_model=CommandResponse)
-async def lauch_acl2_container(container_up: ContainerUp):
+async def lauch_acl2_container(request: Request):
+    raw_body = await request.body()  # Obtiene el cuerpo crudo de la petición
+    decoded_body = raw_body.decode("utf-8", errors="replace")  # Decodifica el cuerpo como UTF-8
+    logger.info(f"Raw input: {repr(decoded_body)}")  # Muestra caracteres ocultos
     """ Endpoint to launch an ACL2 instance"""
-    id = container_up.user_id
+    json_data = json.loads(decoded_body) 
+    id = json_data.get("user_id", "")
     status = True
     if settings == "podman":
         status =await manager.start_acl2_container(id)
@@ -22,10 +27,16 @@ async def lauch_acl2_container(container_up: ContainerUp):
     return status
 
 @container_router.post("/execute/", response_model=CommandResponse)
-async def execute_acl2(command_request: CommandRequest):
+async def execute_acl2(request: Request):
     """ Endpoint to execute an ACL2 command and get the response """
-    cmd = command_request.command
-    id = command_request.user_id
+    
+    raw_body = await request.body()  # Obtiene el cuerpo crudo de la petición
+    decoded_body = raw_body.decode("utf-8", errors="replace")  # Decodifica el cuerpo como UTF-8
+    logger.info(f"Raw input: {repr(decoded_body)}")  # Muestra caracteres ocultos
+    """ Endpoint to launch an ACL2 instance"""
+    json_data = json.loads(decoded_body) 
+    id = json_data.get("user_id", "")
+    cmd = json_data.get("command", "")
     if settings == "podman":
         output = await manager.send_command(cmd, id)
     else:
